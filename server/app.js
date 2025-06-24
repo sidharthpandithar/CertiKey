f7885c2dae2611f893c8294f47509239ffb8acb2
Sidharth Pandithar <138206446+sidharthpandithar@users.noreply.github.com>
2025-06-24 19:42:40 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -17,6 +17,7 @@ var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 
 var app = express();
+app.set("trust proxy", 1);
 
 mongoose
   .connect(process.env.MONGO_URI)
